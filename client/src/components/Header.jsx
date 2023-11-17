5b76fbb438de65e5efa4f30fd20a8ae96cfcb0f0
Paul Kamau <njigupaul@gmail.com>
2023-11-18 00:23:26 +0300

    Set up the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,17 +22,17 @@ export default function Header() {
         </form>
         <ul className="flex gap-4">
           <Link to={"./"}>
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+            <li className="hidden sm:inline text-slate-700 hover:bg-slate-400 hover:text-slate-100 cursor-pointer p-2 rounded-md">
               Home
             </li>
           </Link>
           <Link to={"./About"}>
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+            <li className="hidden sm:inline text-slate-700 hover:bg-slate-400 hover:text-slate-100 cursor-pointer p-2 rounded-md">
               About
             </li>
           </Link>
           <Link to={"./sign-in"}>
-            <li className="sm:inline text-slate-700 hover:underline cursor-pointer">
+            <li className="sm:inline text-slate-700 hover:bg-slate-400 hover:text-slate-100 cursor-pointer p-2 rounded-md">
               Sign in
             </li>
           </Link>
