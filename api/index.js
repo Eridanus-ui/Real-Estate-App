aa29e37f96cd3b32df2d101dca03c6afd88f155c
Paul Kamau <njigupaul@gmail.com>
2023-11-19 18:13:49 +0300

    Created a test API Route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log("Server connected to DataBase");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     console.log("Server connected to DataBase");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!!");
+  console.log("Server is running on port at http://localhost:3000");
 });
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Hello Paul",
+  });
+});
+
+app.use("/api/user", userRouter);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-slate-600" />
+          <FaSearch className="text-slate-600 cursor-pointer" />
         </form>
         <ul className="flex gap-4">
           <Link to={"./"}>
